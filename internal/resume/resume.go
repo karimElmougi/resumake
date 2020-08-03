6201df89c1c8af44310de15c2a7e659cce729956
kpence <pencekyle@gmail.com>
2020-08-03 09:49:46 -0500

    Renamed censoringenabled yaml variable to censoringEnabled (camelCase)
    Because camelCase is idiomatic of yaml

--- internal/resume/resume.go
+++ internal/resume/resume.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Resume struct {
-	CensoringEnabled bool             `yaml:"censoringenabled"`
+	CensoringEnabled bool             `yaml:"censoringEnabled"`
 	Header           Header           `yaml:"header"`
 	EducationEntries []EducationEntry `yaml:"education"`
 	JobEntries       []JobEntry       `yaml:"experience"`
